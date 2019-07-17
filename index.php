180e53f1c6bf26360f4fd5294be4a11b4f5dbd95
galeh <galihdapaimanda@gmail.com>
2019-07-17 13:22:12 +0800

    First Upload

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffee</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="navigasi">
+      <div class="nav-container">
+        <a class="sub-nav" href="#">home</a>
+        <a class="sub-nav" href="#">blog</a>
+        <a class="sub-nav" href="#">about</a>
+        <a class="sub-nav contact" href="#">contact</a>
+      </div>
+    </div>
+    <div class="header-content">
+      <div class="box-left-continer">
+        <div class="fill-container">
+          <svg class="little-rule" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+            width="27" height="27"
+            viewBox="0 0 172 172"
+            style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M74.53333,17.2c-31.59642,0 -57.33333,25.73692 -57.33333,57.33333c0,31.59642 25.73692,57.33333 57.33333,57.33333c13.73998,0 26.35834,-4.87915 36.24766,-12.97839l34.23203,34.23203c1.43802,1.49778 3.5734,2.10113 5.5826,1.57735c2.0092,-0.52378 3.57826,-2.09284 4.10204,-4.10204c0.52378,-2.0092 -0.07957,-4.14458 -1.57735,-5.5826l-34.23203,-34.23203c8.09924,-9.88932 12.97839,-22.50768 12.97839,-36.24766c0,-31.59642 -25.73692,-57.33333 -57.33333,-57.33333zM74.53333,28.66667c25.39937,0 45.86667,20.4673 45.86667,45.86667c0,25.39937 -20.46729,45.86667 -45.86667,45.86667c-25.39937,0 -45.86667,-20.46729 -45.86667,-45.86667c0,-25.39937 20.4673,-45.86667 45.86667,-45.86667z"></path></g></g></svg>
+            <div class="social-icon">
+              <li> <div class="line"></div> </li>
+              <li> <a href="#"> <img src="img/icons8-facebook-f-10.png" alt=""> </a> </li>
+              <li> <a href="#"> <img src="img/icons8-linkedin-2-15.png" alt=""> </a> </li>
+              <li> <a href="#"> <img src="img/icons8-twitter-20.png" alt=""> </a> </li>
+            </div>
+        </div>
+      </div>
+      <img class="header-img-rule" src="https://images.pexels.com/photos/312418/pexels-photo-312418.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="header-img">
+      <div class="header-desc-container">
+        <div class="desc-box">
+          <img class="png-bg" src="img/—Pngtree—coffee_923934.png" alt="">
+          <p class="text-1">Better Life with</p>
+          <p class="text-2">Coff<span style="color: #eacc95;">ee</span> </p>
+          <p class="text-3">Coffee is a brewed drink prepared from roasted coffee beans, the seeds of berries from certain Coffea species. The genus Coffea is native to tropical Africa (specifically having its origin in Ethiopia and Sudan) and Madagascar, the Comoros, Mauritius, and Réunion in the Indian Ocean. Coffee plants are now cultivated in over 70 countries, primarily in the equatorial regions of the Americas, Southeast Asia, Indian subcontinent, and Africa. The two most commonly grown are C. arabica and C. robusta. Once ripe, coffee berries are picked, processed, and dried. Dried coffee seeds (referred to as "beans") are roasted to varying degrees, depending on the desired flavor. Roasted beans are ground and then brewed with near-boiling water to produce the beverage known as coffee.</p>
+        </div>
+      </div>
+    </div>
+    <div class="second-content">
+      <div class="right-boxes">
+        <img class="img-rules" src="img/aroma-art-beautiful-5441132fix.png" alt="">
+      </div>
+      <div class="left-side">
+        <div class="second-content-desc">
+          <p class="second-content-title">Taste the <span style="color: #eacc95">Flavour</span></p>
+          <p class="title-desc">Coffee provides a complex blend of different flavours, which together produce a range of sensory experiences. The sensory profile of a cup of coffee varies according to a number of factors, the type and blend of coffee beans, geographical source, roasting method, and method of preparation.  The variation in these aspects will impact the overall sensory experience obtained from a cup of coffee during both the preparation and consumption of the coffee.</p>
+          <button class="button-style" type="button" name="button">view more</button>
+        </div>
+        <p class="vertical-text">FLAVOUR</p>
+        <p class="vertical-text-2">COFFEE CREAM</p>
+        <div class="line-2"></div>
+      </div>
+    </div>
+    <div class="third-content">
+      <div class="boxes-continer">
+        <div class="kotak">
+          <img class="third-content-img" src="https://images.pexels.com/photos/1233528/pexels-photo-1233528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        </div>
+        <div class="kotak">
+          <p class="kotak-desc">Coffee is darkly colored, bitter, slightly acidic and has a stimulating effect in humans, primarily due to its caffeine content.</p>
+          <a class="read-more" href="#">read more..</a>
+        </div>
+        <div class="kotak">
+          <img class="third-content-img" src="https://images.pexels.com/photos/1207918/pexels-photo-1207918.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+        </div>
+      </div>
+      <div class="boxes-continer">
+        <div class="kotak">
+          <p class="kotak-desc">While coffee is native to Ethiopia and Sudan, the earliest credible evidence of coffee-drinking as the modern beverage appears in modern-day Yemen in southern Arabia in the middle of the 15th century in Sufi shrines.</p>
+          <a class="read-more" href="#">read more..</a>
+        </div>
+        <div class="kotak">
+          <img class="third-content-img" src="https://images.pexels.com/photos/312420/pexels-photo-312420.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        </div>
+        <div class="kotak">
+          <p class="kotak-desc">Coffee plants are now cultivated in over 70 countries, primarily in the equatorial regions of the Americas, Southeast Asia, Indian subcontinent, and Africa.</p>
+          <a class="read-more" href="#">read more..</a>
+        </div>
+      </div>
+      <button class="explore-more" type="button" name="button">explore more</button>
+    </div>
+    <div class="fourth-content">
+      <div class="fourth-img-container">
+        <img class="fourth-content-img" src="https://cdn.pixabay.com/photo/2017/04/25/08/09/coffee-beans-2258874_960_720.jpg" alt="">
+      </div>
+    </div>
+    <div class="fifth-content">
+      <div class="box-mid">
+        <div class="group">
+          <p class="footer-title"> <span style="color: #c8772c;">Interested to</span> Knowing More?</p>
+          <p class="small-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <form class="footer-form" action="index.html" method="post">
+            <input class="footer-textbox" type="text" name="" value="">
+            <button class="footer-button" type="button" name="button">
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+              width="22" height="22"
+              viewBox="0 0 172 172"
+              style=" fill:#000000; margin: 0; padding: 0;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#f9f1e4"><path d="M16.60517,74.046l28.53767,28.53767c2.82367,2.82367 7.32433,3.03867 10.39883,0.50167l76.3465,-62.96633l-62.96633,76.33933c-2.537,3.0745 -2.322,7.58233 0.50167,10.39883l28.53767,28.53767c4.00617,4.00617 10.8145,2.494 12.7495,-2.83083l46.483,-127.83183c2.24317,-6.17767 -3.741,-12.169 -9.91867,-9.91867l-127.839,46.483c-5.32483,1.935 -6.837,8.74333 -2.83083,12.7495z"></path></g></g></svg>
+            </button>
+          </form>
+        </div>
+        <table>
+          <tr>
+            <td style="border-right: 1px solid #c8772c;">
+              <p style="margin-right: 2em;">Coffee</p>
+            </td>
+            <td>
+              <p style="margin-left: 2em;">
+                <img src="https://img.icons8.com/material-rounded/24/000000/facebook-new.png">
+                <img src="https://img.icons8.com/material/24/000000/linkedin.png">
+                <img src="https://img.icons8.com/android/24/000000/twitter.png">
+              </p>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/parallax.min.js" type="text/javascript"></script>
+  <script src="js/script.js" type="text/javascript"></script>
+  </body>
+</html>
